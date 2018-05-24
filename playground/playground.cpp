148262b4882caafeae8c885ed45a2cf37989798d
Aleksi Backman <bddubbb@gmail.com>
2018-05-25 02:29:55 +0300

    Tutorial 2 in progress

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -2,70 +2,94 @@
 #include <stdlib.h>
 
 #include <GL/glew.h>
-
 #include <GLFW/glfw3.h>
-GLFWwindow* window;
 
 #include <glm/glm.hpp>
+
 using namespace glm;
 
-int main( void )
-{
-	// Initialise GLFW
-	if( !glfwInit() )
-	{
-		fprintf( stderr, "Failed to initialize GLFW\n" );
-		getchar();
+int main(void) {
+	static const GLfloat g_vertex_buffer_data[] = {
+		-1.0f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+	};
+
+	// Initialize GLFW
+	glewExperimental = true; // Needed for core profile
+	if (!glfwInit()) {
+		fprintf(stderr, "Failed to initialize GLFW\n");
 		return -1;
 	}
 
-	glfwWindowHint(GLFW_SAMPLES, 4);
-	glfwWindowHint(GLFW_RESIZABLE,GL_FALSE);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // OpenGL 3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	// glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // For MacOS. Should not need
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // No old OpenGL
 
 	// Open a window and create its OpenGL context
-	window = glfwCreateWindow( 1024, 768, "Playground", NULL, NULL);
-	if( window == NULL ){
-		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
-		getchar();
+	GLFWwindow* window;
+	window = glfwCreateWindow( 1024, 768, "Tutorial 01", NULL, NULL);
+	if (NULL == window) {
+		fprintf(stderr, "Failed to open GLFW window. If you have an "
+				"Intel GPU, they are not 3.3 compatible. Try "
+				"the 2.1 version of the tutorial\n");
 		glfwTerminate();
 		return -1;
 	}
-	glfwMakeContextCurrent(window);
 
-	// Initialize GLEW
-	if (glewInit() != GLEW_OK) {
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	// This will identify vertex buffer
+	GLuint vertexbuffer;
+	// Generate 1 buffer, put the resulting identifier in vertexbuffer
+	glGenBuffers(1, &vertexbuffer);
+	// The following commands will talk about 'vertexbuffer' buffer
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	// Give vertices to OpenGL.
+	glBufferData(GL_ARRAY_BUFFER,
+		     sizeof(g_vertex_buffer_data),
+		     g_vertex_buffer_data,
+		     GL_STATIC_DRAW);
+
+	glfwMakeContextCurrent(window); // Initialize GLEW
+	if (GLEW_OK != glewInit()) {
 		fprintf(stderr, "Failed to initialize GLEW\n");
-		getchar();
-		glfwTerminate();
 		return -1;
 	}
 
-	// Ensure we can capture the escape key being pressed below
+	// Ensure we can capture the escape key being pressed
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
-	// Dark blue background
-	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
-
-	do{
+	do {
+		// Clear the screen. It's not mentioned before Tutorial 02,
+		// but it can cause flickering, so it's there nonetheless.
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// Draw nothing, see you in tutorial 2 !
+		// 1st attribute buffer : vertices
+		glEnableVertexAttribArray(0);
+		// glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(
+				0,		// attribute 0. No reason for 0, but must match the layout in shader.
+				3,		// size
+				GL_FLOAT,	// type
+				GL_FALSE,	// normalized?
+				0,		// stride
+				(void*)0	// array buffer offset
+		);
+
+		// Draw magnificent triangle
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDisableVertexAttribArray(0);
 
 		// Swap buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
-
-	} // Check if the ESC key was pressed or the window was closed
-	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-		   glfwWindowShouldClose(window) == 0 );
-
-	// Close OpenGL window and terminate GLFW
-	glfwTerminate();
+	} while((GLFW_PRESS != glfwGetKey(window, GLFW_KEY_ESCAPE)) &&
+		(0 == glfwWindowShouldClose(window)));
 
 	return 0;
 }
-
